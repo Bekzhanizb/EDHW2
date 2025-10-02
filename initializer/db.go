1b902f419bf815dba9d83f853428cc8d3be900e1
Bekezhan <bekzhanizbasar228@gmail.com>
2025-10-02 23:23:22 +0500

    Changed initializer logic

---
+++ initializer/db.go
@@ -0,0 +1,42 @@
+package initializer
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDatabase() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		AppConfig.DBHost,
+		AppConfig.DBUser,
+		AppConfig.DBPassword,
+		AppConfig.DBName,
+		AppConfig.DBPort,
+		AppConfig.DBSSLMode,
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database:", err)
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatal("❌ Failed to get sql.DB:", err)
+	}
+
+	// connection pool
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Println("✅ Database connected")
+}
--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"database/sql"
-	"log"
-)
-
 func main() {
-	connect := getDSNFromEnv()
-	db, err := sql.Open("postgres", connect)
-	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
-	}
-	defer db.Close()
 
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(5)
-
-	if err := db.Ping(); err != nil {
-		log.Fatalf("Error pinging database: %v", err)
-	}
-	if err := InitSQLSchema(db); err != nil {
-		log.Fatalf("Error initializing scheme: %v", err)
-	}
-}
-func InitSQLSchema(db *sql.DB) error {
-	q := `
-	CREATE TABLE IF NOT EXISTS users (
-		id SERIAL PRIMARY KEY,
-		name TEXT UNIQUE NOT NULL,
-		age INTEGER NOT NULL
-	);
-	`
-	_, err := db.Exec(q)
-	return err
 }
